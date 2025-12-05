7009e4de7d86493239351544c1d8ab70983efd93
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-05 09:45:52 +0100

    Feat: aggiunto bootstrap, aggiunto axios, e iniziata struttura di base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const post = {
+  author : "",
+  title : "",
+  body : "",
+  public : "",
+
+}
 
 function App() {
   
 
+
   return (
     <>
-      
+      <div className='container'></div>
     </>
   )
 }
